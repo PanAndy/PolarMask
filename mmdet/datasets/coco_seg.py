0421f03a66ad4cbf7bdfe7a17a2e47e9fcc53737
PanAndy <1163962054@qq.com>
2020-03-31 14:22:35 +0800

    annotations for coco_seg.py reading and comprehension

--- mmdet/datasets/coco_seg.py
+++ mmdet/datasets/coco_seg.py
@@ -34,6 +34,12 @@ INF = 1e8
 
 
 def get_angle(v1, v2=[0,0,100,0]):
+    """
+    calculate the angle between two vectors.
+    :param v1:vector, v1=[x1',y1',x2',y2']
+    :param v2:the reference vector, v2=[x1,y1,x2,y2]
+    :return:
+    """
     dx1 = v1[2] - v1[0]
     dy1 = v1[3] - v1[1]
     dx2 = v2[2] - v2[0]
@@ -99,23 +105,30 @@ class Coco_Seg_Dataset(CustomDataset):
         return valid_inds
 
     def _parse_ann_info(self, ann_info, with_mask=True):
-        """Parse bbox and mask annotation.
+        """Parse bbox and mask annotation. for detection application.
 
         Args:
-            ann_info (list[dict]): Annotation info of an image.
+            ann_info (list[dict]): Annotation info of an image, in coco type. not an anno, but a list of.
             with_mask (bool): Whether to parse mask annotations.
 
         Returns:
-            dict: A dict containing the following keys: bboxes, bboxes_ignore,
+            dict: A dict containing the following keys: bboxes, bboxes_ignore.
+            They are lists, containing the not too small instances annos.
                 labels, masks, mask_polys, poly_lens.
+                bboxes: the detection bboxes, (x1,y1,x2,y2),left-top coordinates and right-bottom coordinates
+                bboxes_ignore: what's this key used for?
+                labels: the label codes for detection application
+                masks: binary mask
+                mask_polys: polygon mask
+                poly_len:: ploygon mask length.
         """
         gt_bboxes = []
         gt_labels = []
         gt_bboxes_ignore = []
         # Two formats are provided.
         # 1. mask: a binary map of the same size of the image.
-        # 2. polys: each mask consists of one or several polys, each poly is a
-        # list of float.
+        # 2. polys: each mask consists of one or several polys, each poly is a list of float.
+        # this means there are two mask formats in ann: binary mask and polys mask.
 
 
         self.debug = False
@@ -138,18 +151,19 @@ class Coco_Seg_Dataset(CustomDataset):
 
             if ann['area'] <= 15 or (w < 10 and h < 10) or self.coco.annToMask(ann).sum() < 15:
                 # print('filter, area:{},w:{},h:{}'.format(ann['area'],w,h))
+                # filter instances, too small.
                 if self.debug:
                     count+=1
                 continue
 
             bbox = [x1, y1, x1 + w - 1, y1 + h - 1]
-            if ann['iscrowd']:
+            if ann['iscrowd']:  # iscrowd is true, means there are many objects for this instance, eg. under occlusion.
                 gt_bboxes_ignore.append(bbox)
             else:
                 gt_bboxes.append(bbox)
-                gt_labels.append(self.cat2label[ann['category_id']])
+                gt_labels.append(self.cat2label[ann['category_id']])  # 'category_name' to label number for detection.
             if with_mask:
-                gt_masks.append(self.coco.annToMask(ann))
+                gt_masks.append(self.coco.annToMask(ann))   # mask anns must be converted into binary mask for use.
                 mask_polys = [
                     p for p in ann['segmentation'] if len(p) >= 6
                 ]  # valid polygons have >= 3 points (6 coordinates)
@@ -182,15 +196,35 @@ class Coco_Seg_Dataset(CustomDataset):
         return ann
 
     def prepare_train_img(self, idx):
+        """
+        get image according to the idx.
+        and convert the mask to polar mask.
+        :param idx:
+        :return:data,
+        a dict contains img, img_meta,gt_bboxes,gt_labels,gt_masks,gt_bboxes_ignore,_gt_labels,_gt_bboxes,_gt_masks
+        """
+        # the img_info comes from coco annotation file. initial when the class constructs.
         img_info = self.img_infos[idx]
+
+        # image size is (w,h,3). this is different from torch.
         img = mmcv.imread(osp.join(self.img_prefix, img_info['filename']))
-        # corruption
+
+        learning = False
+        if learning:
+            img_id = self.img_infos[idx]['id']
+            ann_ids = self.coco.getAnnIds(imgIds=[img_id])
+            ann_info = self.coco.loadAnns(ann_ids)
+            plt.imshow(img);plt.axis('off')
+            self.coco.showAnns(ann_info)
+            plt.show()
+
+        # corruption, used for augmentation the image, for degrade the image quality.
         if self.corruption is not None:
             img = corrupt(
                 img,
                 severity=self.corruption_severity,
                 corruption_name=self.corruption)
-        # load proposals if necessary
+        # load proposals if necessary. proposals is for what???
         if self.proposals is not None:
             proposals = self.proposals[idx][:self.num_max_proposals]
             # TODO: Handle empty proposals properly. Currently images with
@@ -208,7 +242,7 @@ class Coco_Seg_Dataset(CustomDataset):
             else:
                 scores = None
 
-        ann = self.get_ann_info(idx)
+        ann = self.get_ann_info(idx)    # ann has been converted for detection application.bbox(x1,y1,x2,y2)
 
         gt_bboxes = ann['bboxes']
         gt_labels = ann['labels']
@@ -226,7 +260,7 @@ class Coco_Seg_Dataset(CustomDataset):
         # randomly sample a scale
         img_scale = random_scale(self.img_scales, self.multiscale_mode)
         img, img_shape, pad_shape, scale_factor = self.img_transform(img, img_scale, flip, keep_ratio=self.resize_keep_ratio)
-
+        # img size (3,w,h). And the img has been normalized, which means it can't be shown.
 
         img = img.copy()
         if self.with_seg:
@@ -272,7 +306,6 @@ class Coco_Seg_Dataset(CustomDataset):
         if self.with_mask:
             data['gt_masks'] = DC(gt_masks, cpu_only=True)
 
-
         #--------------------offline ray label generation-----------------------------
 
         self.center_sample = True
@@ -283,15 +316,15 @@ class Coco_Seg_Dataset(CustomDataset):
         featmap_sizes = self.get_featmap_size(pad_shape)
         self.featmap_sizes = featmap_sizes
         num_levels = len(self.strides)
-        all_level_points = self.get_points(featmap_sizes)
+        all_level_points = self.get_points(featmap_sizes)   # receptive filed map [feature map,img_field]
         self.num_points_per_level = [i.size()[0] for i in all_level_points]
-
+        # the point numbers of each level of feature map
         expanded_regress_ranges = [
             all_level_points[i].new_tensor(self.regress_ranges[i])[None].expand_as(
                 all_level_points[i]) for i in range(num_levels)
         ]
         concat_regress_ranges = torch.cat(expanded_regress_ranges, dim=0)
-        concat_points = torch.cat(all_level_points, 0)
+        concat_points = torch.cat(all_level_points, 0)  # convert all feature points to a vector, size = 20460
         gt_masks = gt_masks[:len(gt_bboxes)]
 
         gt_bboxes = torch.Tensor(gt_bboxes)
@@ -310,6 +343,11 @@ class Coco_Seg_Dataset(CustomDataset):
         return data
 
     def get_featmap_size(self, shape):
+        """
+        feature map size = (image size) / (stride), according the math relationship.
+        :param shape:
+        :return:
+        """
         h,w = shape[:2]
         featmap_sizes = []
         for i in self.strides:
@@ -317,6 +355,13 @@ class Coco_Seg_Dataset(CustomDataset):
         return featmap_sizes
 
     def get_points(self, featmap_sizes):
+        '''
+        this function construct a list of map.
+        a level in list map a point in the corresponding feature map to the image square center.
+        it looks like a list of receptive filed map.
+        :param featmap_sizes: a list of feature map size.
+        :return:
+        '''
         mlvl_points = []
         for i in range(len(featmap_sizes)):
             mlvl_points.append(
@@ -324,6 +369,13 @@ class Coco_Seg_Dataset(CustomDataset):
         return mlvl_points
 
     def get_points_single(self, featmap_size, stride):
+        """
+        this function construct a map, which map a point in the feature map to the image square center
+        it looks like receptive field map
+        :param featmap_size:
+        :param stride:
+        :return:
+        """
         h, w = featmap_size
         x_range = torch.arange(
             0, w * stride, stride)
@@ -332,9 +384,20 @@ class Coco_Seg_Dataset(CustomDataset):
         y, x = torch.meshgrid(y_range, x_range)
         points = torch.stack(
             (x.reshape(-1), y.reshape(-1)), dim=-1) + stride // 2
+        # x.reshape(-1) converts the 2-d x to 1-d vector,
+        # +stride//2 make the map point correspond to the stride*stride square center
         return points.float()
 
     def polar_target_single(self, gt_bboxes, gt_masks, gt_labels, points, regress_ranges):
+        '''
+
+        :param gt_bboxes:
+        :param gt_masks:
+        :param gt_labels:
+        :param points:
+        :param regress_ranges:
+        :return:
+        '''
         num_points = points.size(0)
         num_gts = gt_labels.size(0)
         if num_gts == 0:
@@ -348,7 +411,7 @@ class Coco_Seg_Dataset(CustomDataset):
         areas = areas[None].repeat(num_points, 1)
         regress_ranges = regress_ranges[:, None, :].expand(
             num_points, num_gts, 2)
-        gt_bboxes = gt_bboxes[None].expand(num_points, num_gts, 4)
+        gt_bboxes = gt_bboxes[None].expand(num_points, num_gts, 4)  # (20460,2,4), the 2 represents 2 instances.
         #xs ys 分别是points的x y坐标
         xs, ys = points[:, 0], points[:, 1]
         xs = xs[:, None].expand(num_points, num_gts)
@@ -357,7 +420,7 @@ class Coco_Seg_Dataset(CustomDataset):
         right = gt_bboxes[..., 2] - xs
         top = ys - gt_bboxes[..., 1]
         bottom = gt_bboxes[..., 3] - ys
-        bbox_targets = torch.stack((left, top, right, bottom), -1)   #feature map上所有点对于gtbox的上下左右距离 [num_pix, num_gt, 4]
+        bbox_targets = torch.stack((left, top, right, bottom), -1)   # fcos' representation method. feature map上所有点对于gtbox的上下左右距离 [num_pix, num_gt, 4]
 
 
 
@@ -404,21 +467,21 @@ class Coco_Seg_Dataset(CustomDataset):
         # condition2: limit the regression range for each location
         max_regress_distance = bbox_targets.max(-1)[0]
 
-        inside_regress_range = (
+        inside_regress_range = (    # each point has a regress range, if a point is in the range , result is true.
             max_regress_distance >= regress_ranges[..., 0]) & (
             max_regress_distance <= regress_ranges[..., 1])
 
         areas[inside_gt_bbox_mask == 0] = INF
-        areas[inside_regress_range == 0] = INF
+        areas[inside_regress_range == 0] = INF  # if a point is not in bt_bbox_mask and regress range ,it is INF.
         min_area, min_area_inds = areas.min(dim=1)
 
-        labels = gt_labels[min_area_inds]
+        labels = gt_labels[min_area_inds]   # label each point for multi-instances.
         labels[min_area == INF] = 0         #[num_gt] 介于0-80
 
         bbox_targets = bbox_targets[range(num_points), min_area_inds]
         pos_inds = labels.nonzero().reshape(-1)
 
-        mask_targets = torch.zeros(num_points, 36).float()
+        mask_targets = torch.zeros(num_points, 36).float() # initial polar mask representation.
 
         pos_mask_ids = min_area_inds[pos_inds]
         for p,id in zip(pos_inds, pos_mask_ids):
@@ -431,6 +494,16 @@ class Coco_Seg_Dataset(CustomDataset):
         return labels, bbox_targets, mask_targets
 
     def get_sample_region(self, gt, strides, num_points_per, gt_xs, gt_ys, radius=1):
+        '''
+
+        :param gt:
+        :param strides:
+        :param num_points_per:
+        :param gt_xs:
+        :param gt_ys:
+        :param radius:
+        :return:
+        '''
         center_x = (gt[..., 0] + gt[..., 2]) / 2
         center_y = (gt[..., 1] + gt[..., 3]) / 2
         center_gt = gt.new_zeros(gt.shape)
@@ -462,6 +535,21 @@ class Coco_Seg_Dataset(CustomDataset):
         return inside_gt_bbox_mask
 
     def get_mask_sample_region(self, gt_bb, mask_center, strides, num_points_per, gt_xs, gt_ys, radius=1):
+        '''
+
+        :param gt_bb: a tensor(20460,number of instances,4),
+                      20460 is the total point number in all [8, 16, 32, 64, 128] feature maps
+                      for each instance, the gt_bb at each feature map point is same the ground truth bbox.
+        :param mask_center: a tensor(20460,number of instances,2), the same as gt_bb
+        :param strides:[8, 16, 32, 64, 128]
+        :param num_points_per: each level feature map number points
+        :param gt_xs: a tensor(20460, number of instances),
+                        a value ,index is the point at feature map, value is the point at image
+        :param gt_ys: the same the gt_ys
+        :param radius: sample region. if a point is at the same region, it is a positive example.
+        :return:inside_gt_bbox_mask,
+                a tensor(20460,number of instances) tensor.bool, whether a point is in mask and box region.
+        '''
         center_y = mask_center[..., 0]
         center_x = mask_center[..., 1]
         center_gt = gt_bb.new_zeros(gt_bb.shape)
@@ -469,7 +557,7 @@ class Coco_Seg_Dataset(CustomDataset):
         if center_x[..., 0].sum() == 0:
             return gt_xs.new_zeros(gt_xs.shape, dtype=torch.uint8)
 
-        beg = 0
+        beg = 0  # get each level feature map points, according to the index relationship.
         for level,n_p in enumerate(num_points_per):
             end = beg + n_p
             stride = strides[level] * radius
@@ -483,16 +571,21 @@ class Coco_Seg_Dataset(CustomDataset):
             center_gt[beg:end, :, 2] = torch.where(xmax > gt_bb[beg:end, :, 2], gt_bb[beg:end, :, 2], xmax)
             center_gt[beg:end, :, 3] = torch.where(ymax > gt_bb[beg:end, :, 3], gt_bb[beg:end, :, 3], ymax)
             beg = end
-
+        # if look a point as the mass center, calculate the point to left gt_bbox four sides distance.
         left = gt_xs - center_gt[..., 0]
         right = center_gt[..., 2] - gt_xs
         top = gt_ys - center_gt[..., 1]
         bottom = center_gt[..., 3] - gt_ys
-        center_bbox = torch.stack((left, top, right, bottom), -1)
+        center_bbox = torch.stack((left, top, right, bottom), -1)   # it should be the regression target of fcos.
         inside_gt_bbox_mask = center_bbox.min(-1)[0] > 0  # 上下左右都>0 就是在bbox里面
         return inside_gt_bbox_mask
 
     def get_centerpoint(self, lis):
+        '''
+        get the mass center by the contour stored in the lis
+        :param lis:
+        :return: center->(x,y)
+        '''
         area = 0.0
         x, y = 0.0, 0.0
         a = len(lis)
@@ -515,6 +608,11 @@ class Coco_Seg_Dataset(CustomDataset):
         return [int(x), int(y)]
 
     def get_single_centerpoint(self, mask):
+        """
+        calculate the mass center
+        :param mask:
+        :return: (center, contour)
+        """
         contour, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         contour.sort(key=lambda x: cv2.contourArea(x), reverse=True) #only save the biggest one
         '''debug IndexError: list index out of range'''
@@ -532,6 +630,13 @@ class Coco_Seg_Dataset(CustomDataset):
         return center, contour
 
     def get_36_coordinates(self, c_x, c_y, pos_mask_contour):
+        """
+
+        :param c_x:
+        :param c_y:
+        :param pos_mask_contour:
+        :return:
+        """
         ct = pos_mask_contour[:, 0, :]
         x = ct[:, 0] - c_x
         y = ct[:, 1] - c_y
@@ -587,6 +692,19 @@ class Coco_Seg_Dataset(CustomDataset):
 
 
     def __getitem__(self, idx):
+        '''
+        :param idx: idx is the image index in COCO dataset.
+        once you get a index of image from coco, it generates another different random idx.
+        :return: data is a dict, which contains
+                 img:
+                 img_meta
+                 gt_bboxes
+                 gt_labels
+                 gt_masks
+                 _gt_labels, the three is the label,boxe and mask of each instance in polar representation.
+                 _gt_bboxes
+                 _gt_mask
+        '''
         if self.test_mode:
             return self.prepare_test_img(idx)
         while True:
